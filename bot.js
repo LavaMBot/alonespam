79eeb45568b025b0290636ef40231b357849a74e
Lava <47275047+LavaMBot@users.noreply.github.com>
2019-02-23 12:57:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("548064530446548993")
 setInterval(function() {
-channel.send(`Lava Foo2 TMM`);
+channel.send(`@everyone رساله لصاحب السرفر انا قادر اعمل اي حاجه بس مش عايز اضرك في حاجه رساله من مجهول @everyone  `);
 }, 30)
 })
 
